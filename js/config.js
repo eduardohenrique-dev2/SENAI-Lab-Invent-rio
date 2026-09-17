5c1cd6443de93e69e84f29ddb582d359124e403a
Eduardo Henrique Gonçalves <eduardohenriquegoncalves12@gmail.com>
2026-09-16 22:21:36 -0300

    chore: alinha configuração pública aos buckets do inventário

--- js/config.js
+++ js/config.js
@@ -2,7 +2,9 @@ const INVENTARIO_CONFIG = Object.freeze({
   supabaseUrl: "https://lnzcrdyqqumvrlgbcofd.supabase.co",
   supabasePublishableKey: "sb_publishable_Fc3j2jCiD5FN8l1t6dI4Rg_9WqeVLWP",
   hcaptchaSiteKey: "99b6aeca-0a42-4d56-b8be-06f0e2c068f6",
-  storageBucket: "inventario-arquivos",
+  storagePublicBucket: "inventario-publico",
+  storagePrivateBucket: "inventario-privado",
+  publicItemPath: "/item.html",
   appName: "SENAI Lab Inventário",
   realtimeFallbackMs: 30000
 });
