1bcb9b9dd7f151a057a133d52fcf07c06b91d7ee
Eduardo Henrique Gonçalves <eduardohenriquegoncalves12@gmail.com>
2026-09-17 20:45:20 -0300

    chore: invalida cache do importador 2.2

--- js/config.js
+++ js/config.js
@@ -87,9 +87,9 @@ function obterInventarioSupabase() {
     document.head.appendChild(style);
   }
 
-  if (!document.querySelector('script[src="./js/importacoes-v2.js?v=20260917-21"]')) {
+  if (!document.querySelector('script[src="./js/importacoes-v2.js?v=20260917-22"]')) {
     const script = document.createElement("script");
-    script.src = "./js/importacoes-v2.js?v=20260917-21";
+    script.src = "./js/importacoes-v2.js?v=20260917-22";
     script.defer = true;
     document.head.appendChild(script);
   }
